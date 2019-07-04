7243eddfd142fc9f05b2917071f88da91fa68d54
Matheus Reis Silva <matheusreissilva800@gmail.com>
2019-07-04 18:59:57 -0300

    Primeiro Deploy

--- app/controllers/produtos_controller.rb
+++ app/controllers/produtos_controller.rb
@@ -1,25 +1,62 @@
 class ProdutosController < ApplicationController
 
-	def index
-		@produtos = Produto.order(nome: :asc).limit 6
-		@produto_com_desconto = Produto.order(:preco).limit 1
-	end
+    before_action :set_produto, only: [:edit, :update, :destroy]
 
-	def create
-		produto = params.require(:produto).permit(:nome, :descricao, :preco, :quantidade)
-		Produto.create produto
-		redirect_to root_url
-	end
+    def index
+        @produtos = Produto.order(nome: :asc).limit 6
+        @produto_com_desconto = Produto.order(:preco).limit 1
+    end
+    
+    def new
+        @produto = Produto.new
+        @departamentos = Departamento.all
+    end
+    
+    def edit
+        renderiza
+    end
 
-	def destroy
-		id = params[:id]
-		Produto.destroy id
-		redirect_to root_url
-	end
+    def update
+        if @produto.update produto_params
+            flash[:notice] = "Produto atualizado com sucesso!"
+            redirect_to root_url
+        else
+            renderiza
+        end
+    end
+    
+    def create
+        @produto = Produto.new produto_params
+        if @produto.save
+            flash[:notice] = "Produto salvo com sucesso!"
+            redirect_to root_url
+        else
+            renderiza
+        end
+    end
 
-	def busca
-		@nome = params[:nome]
-		@produtos = Produto.where "nome like ?", "%#{@nome}%"
-	end
+    def destroy
+        @produto.destroy
+        redirect_to root_url
+    end
+    
+    def busca
+        @nome = params[:nome]
+        @produtos = Produto.where "nome like ?", "%#{@nome}%"
+    end
+    
+    def produto_params
+        params.require(:produto).permit(:nome, :descricao, :preco, :quantidade, :departamento_id)
+    end
 
+    def set_produto
+        @produto = Produto.find(params[:id])
+    end
+    
+    def renderiza
+        @departamentos = Departamento.all 
+        render :new
+    end
+    
+    
 end
